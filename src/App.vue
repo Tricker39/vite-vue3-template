c81546225bc2d9e09686adfab0dd7e8918c23fe1
Mascot <873132083@qq.com>
2023-07-06 15:26:35 +0800

    feat ✨: 图标 icon 另一种渲染方式

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <div color="red">{{ appStore.userName }}</div>
-    <div v-for="item in arr" :class="item" />
+    <div v-for="item in arr" :class="item">谷歌</div>
+    <VNode :content="divEl" />
   </div>
 </template>
 <script setup lang="ts">
-  // import { ref } from 'vue';
+  import { h } from 'vue';
   import { useAppStore } from '@/store/modules/appStore';
+  import { VNode } from '@/VNode';
   const appStore = useAppStore();
   const arr = [
     'icon-park-twotone-search',
     'icon-park-solid-like',
     'icon-park-outline-search',
     'icon-park-solid-config',
+    'icon-park-outline-zoom-in',
   ];
+  const zoomOut = 'icon-park-outline-zoom-out';
+  const divEl = h('div', { class: zoomOut });
 </script>
 <style scoped></style>
